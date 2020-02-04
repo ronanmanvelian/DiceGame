53f1e4f3adcc401f6c3813907d33f26457463ec3
Ronan <manvelia@bc.edu>
2020-02-04 18:17:23 -0500

    Minor Fixes

--- diceGame.py
+++ diceGame.py
@@ -4,6 +4,7 @@ probability_of_sum_a = 0
 probability_of_sum_b = 0
 d = 0
 
+
 def intro_message():
     print()
     print("Welcome to the Dice Gamble Game! Your goal is to predict a sum")
@@ -12,7 +13,7 @@ def intro_message():
     print("You can either play now or run some simulations to get a feel")
     print("for which sums are most likely to occur.")
     print()
-    print("If you want to play now, enter P. If you first want to run some")
+    print("If you want to play now, enter P. If you want to first run some")
     print("simulations, enter S.")
     print()
 
@@ -102,11 +103,11 @@ def run_simulations():
 
 def play_game():
     possible_num_of_dice = [3, 4, 5, 6, 7, 8]
-    dice_num_selection = numpy.random.randint(0, len(possible_num_of_dice) - 1)
+    dice_num_selection = numpy.random.randint(0, len(possible_num_of_dice))
     num_of_dice = possible_num_of_dice[dice_num_selection]
 
     possible_sides_of_dice = [6, 8, 10, 12, 14, 16]
-    dice_sides_selection = numpy.random.randint(0, len(possible_sides_of_dice) - 1)
+    dice_sides_selection = numpy.random.randint(0, len(possible_sides_of_dice))
     sides_of_dice = possible_sides_of_dice[dice_sides_selection]
 
     opponents = ["Mark", "Alice", "Ronan", "Sydney", "John", "Brianna", "Bill", "Taylor", "Joe", "Ashley"]
@@ -133,13 +134,13 @@ def play_game():
     largest_possible_sum = num_of_dice * sides_of_dice
     third_of_largest_possible_sum = largest_possible_sum // 3
 
-    superb_player_sum = numpy.random.randint((largest_possible_sum // 2) + 1, ((largest_possible_sum + num_of_dice) // 2) + 1)
+    champ_sum = numpy.random.randint((largest_possible_sum // 2) + 1, ((largest_possible_sum + num_of_dice) // 2) + 1)
     great_player_sum = numpy.random.randint(largest_possible_sum // 2, ((largest_possible_sum + num_of_dice) // 2) + 2)
     good_player_sum = numpy.random.randint(third_of_largest_possible_sum, third_of_largest_possible_sum * 2)
     average_player_sum = numpy.random.randint(num_of_dice, largest_possible_sum + 1)
 
     if opponent_trait == "came prepared":
-        opponent_sum_choice = superb_player_sum
+        opponent_sum_choice = champ_sum
     elif opponent_trait == "gives you a little smirk":
         opponent_sum_choice = great_player_sum
     elif opponent_trait == "is feeling confident today":
